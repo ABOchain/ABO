291d335f0471d5e6bf226be35bcb3cdaabc68d05
mjc <myungjae92@gmail.com>
2018-07-16 21:01:08 +0900

    bloodType 관련 함수 추가

--- contracts/ABO.sol
+++ contracts/ABO.sol
@@ -13,26 +13,54 @@ contract ABO {
     constructor() public {
         owner = msg.sender;
         bloodPack.availBlood[1] = true;
-        bloodPack.validTime = 1; 
+        bloodPack.validTime = now; 
     }
 
     modifier restricted() {
         if (msg.sender != owner) _;
     }
 
-    function setBloodInfo(uint bloodInfo) public restricted {
-        bloodPack.bloodInfo = bloodInfo;
-    }
-
     function getBloodInfo() public returns(uint) {
         return bloodPack.bloodInfo;
     }
 
-    function setBloodPackID(bytes bloodPackID) public restricted{
-        bloodPack.bloodPackID = keccak256(bloodPackID);
-    }
-
     function getBloodPackID() public returns(bytes32) {
         return bloodPack.bloodPackID;
+    }
+
+    function setBloodType(uint bloodType) internal {
+        if ( bloodType == 0 ){
+            bloodPack.availBlood[0] = true;
+            bloodPack.availBlood[3] = true;
+        }
+        else if ( bloodType == 1 ){
+            bloodPack.availBlood[1] = true;
+            bloodPack.availBlood[3] = true;
+        }
+        else if ( bloodType == 3 ){
+            bloodPack.availBlood[3] = true;
+        } 
+        else if ( bloodType == 2 ){
+            for (uint idx = 0; idx < 4; idx ++){
+                bloodPack.availBlood[idx] = true;
+            }
+        }
+    }
+    
+    function getBloodType() public returns(uint) {
+        if ( bloodPack.availBlood[0] && bloodPack.availBlood[1] && bloodPack.availBlood[2] && bloodPack.availBlood[3] ){
+            return 2;
+        }
+        else if ( bloodPack.availBlood[0] && bloodPack.availBlood[3] ) {
+            return 0;
+        }
+        else if ( bloodPack.availBlood[1] && bloodPack.availBlood[3] ) {
+            return 1;
+        }
+        else{
+            return 3;
+        }
+
+        return 5;
     }
 }
