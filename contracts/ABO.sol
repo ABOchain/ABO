56081341869186ac5298a6a46e4d0a56ebde0803
mjc <myungjae92@gmail.com>
2018-07-20 17:34:00 +0900

    구조 변경

--- contracts/ABO.sol
+++ contracts/ABO.sol
@@ -1,76 +1,49 @@
 pragma solidity ^0.4.24;
 
 contract ABO {
-    struct BloodPack {
-        bool[4] availBlood;
-        uint bloodInfo;
-        bytes32 bloodPackID;
-        uint validTime;
+    struct BloodDoc {
+        string bloodDocID;
+        uint bloodingType;
+        uint bloodAmount;
+        uint regDate;
     }
     address owner;
-    BloodPack bloodPack;
+    BloodDoc bloodDoc;
 
     constructor() public {
         owner = msg.sender;
-        bloodPack.validTime = now; 
+        bloodDoc.regDate = now; 
     }
 
     modifier restricted() {
         if (msg.sender != owner) _;
     }
 
-    function setBloodInfo(uint bloodInfo) public restricted {
-        bloodPack.bloodInfo = bloodInfo;
+    function setBloodDocID(string bloodDocID) public restricted {
+        bloodDoc.bloodDocID = bloodDocID;
     }
 
-    function getBloodInfo() public view returns(uint) {
-        if (bloodPack.bloodInfo > 1) {
-            return 3;
-        }
-        return bloodPack.bloodInfo;
+    function getBloodDocID() public view restricted returns(string) {
+        return bloodDoc.bloodDocID;
     }
 
-    function setBloodPackID(string bloodPackID) public restricted {
-        bloodPack.bloodPackID = keccak256(abi.encodePacked(bloodPackID));
-    }
-
-    function getBloodPackID() public view restricted returns(bytes32) {
-        return bloodPack.bloodPackID;
-    }
-
-    function setBloodType(uint bloodType) public restricted {
-        if ( bloodType == 0 ){
-            bloodPack.availBlood[0] = true;
-            bloodPack.availBlood[3] = true;
-        }
-        else if ( bloodType == 1 ){
-            bloodPack.availBlood[1] = true;
-            bloodPack.availBlood[3] = true;
-        }
-        else if ( bloodType == 3 ){
-            bloodPack.availBlood[3] = true;
-        } 
-        else if ( bloodType == 2 ){
-            for (uint idx = 0; idx < 4; idx ++){
-                bloodPack.availBlood[idx] = true;
-            }
-        }
+    function setBloodingType(uint bloodingType) public restricted {
+        bloodDoc.bloodingType = bloodingType;
     }
     
-    function getBloodType() public view returns(uint) {
-        if ( bloodPack.availBlood[0] && bloodPack.availBlood[1] && bloodPack.availBlood[2] && bloodPack.availBlood[3] ){
-            return 2;
-        }
-        else if ( bloodPack.availBlood[0] && bloodPack.availBlood[3] ) {
-            return 0;
-        }
-        else if ( bloodPack.availBlood[1] && bloodPack.availBlood[3] ) {
-            return 1;
-        }
-        else{
-            return 3;
-        }
+    function getBloodingType() public view returns(uint) {
+        return bloodDoc.bloodingType;
+    }
 
-        return 5;
+    function setBloodAmount(uint bloodAmount) public restricted {
+        bloodDoc.bloodAmount = bloodAmount;
+    }
+
+    function getBloodAmount() public view returns(uint) {
+        return bloodDoc.bloodAmount;
+    }
+
+    function getRegDate() public view returns(uint) {
+        return bloodDoc.regDate;
     }
 }
