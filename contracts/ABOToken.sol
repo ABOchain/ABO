ad96e4bf873d2c3cc54fc95a305cfac58efdbd04
mjc <myungjae92@gmail.com>
2018-07-28 14:09:52 +0900

    ABOToken Adding

--- contracts/ABOToken.sol
+++ contracts/ABOToken.sol
@@ -5,17 +5,18 @@ import "./token/StandardToken.sol";
 
 
 /**
- * @title SimpleToken
+ * @title ABOToken
  * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `StandardToken` functions.
  */
-contract SimpleToken is StandardToken {
-    string public constant name = "SimpleToken";
-    string public constant symbol = "SIM";
+contract ABOToken is StandardToken {
+    string public constant name = "ABOToken";
+    string public constant symbol = "ABOT";
     uint8 public constant decimals = 18;
 
-    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
+    // 토큰발행량 * 변환값
+    uint256 public constant INITIAL_SUPPLY = 1 * (10 ** uint256(decimals));
 
     /**
     * @dev Constructor that gives msg.sender all of existing tokens.
