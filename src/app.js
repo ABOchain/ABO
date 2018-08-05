2d88733cb3b52b23e84601921d085e591dafd9bf
mjc <myungjae92@gmail.com>
2018-08-05 14:35:45 +0900

    commit

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ var Static = require("./js/static.js");
 var aboJson = require(Path.join(__dirname, '../build/contracts/ABO.json'));
 var aboTokenJson = require(Path.join(__dirname, '../build/contracts/ABOToken.json'));
 
-var web3 = new Web3(new Web3.providers.HttpProvider("http://211.249.62.37:8545"));
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 var aboContract = Contract(aboJson);
 var aboTokenContract = Contract(aboTokenJson);
@@ -29,7 +29,7 @@ var app = Express();
 var server = Http.createServer(app);
 
 app.use(BodyParser.json())
-server.listen(8080);
+server.listen(Static.HTTP_PORT);
 
 // API Method
 
