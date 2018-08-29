7d768d7625114d709960301c704b80b517d639ff
mjc <myungjae92@gmail.com>
2018-08-29 15:24:26 +0900

    토큰 거래 루틴 수정

--- src/app.js
+++ src/app.js
@@ -50,7 +50,7 @@ app.post("/create/bloodDoc", async (req, res, next) => {
     await aboContract.new(bloodAgency, bloodDocID, bloodingType, bloodAmount, regTime, {from : Static.ORIGIN_ADDR, gas : 7412340}).then( abo => {
         var contactAddress = abo.address;
         var txHash = abo.transactionHash;
-        
+
         res.status(Static.HTTP_OK).send({ contactAddress, txHash });
     });
 });
@@ -63,7 +63,7 @@ app.post("/create/account", async (req, res, next) => {
     var password = decodeData.password;
 
     var account = await web3.personal.newAccount(password);
-    
+
     res.status(Static.HTTP_OK).send({account});
 });
 
@@ -86,8 +86,8 @@ app.post("/create/initToken", (req, res, next) => {
     aboTokenContract.deployed().then( aboToken => {
         aboToken.transfer(addr, value, {from : Static.ORIGIN_ADDR}).then( result => {
             res.status(Static.HTTP_OK).send({txHash : result.tx});
-        })
-    })
+        });
+    });
 });
 
 app.post("/create/sendToken", (req, res, next) => {
@@ -106,18 +106,22 @@ app.post("/create/sendToken", (req, res, next) => {
     var fromAddr = decodeData.fromAddr;
     var toAddr = decodeData.toAddr;
     var value = parseInt(decodeData.value);
-    
+
     web3.personal.unlockAccount(Static.ORIGIN_ADDR, Static.ORIGIN_ADDR_PASS, 60);
 
+    if (web3.eth.getBalance(fromAddr) < 148246800000000000)
+        web3.eth.sendTransaction({from: Static.ORIGIN_ADDR, to: fromAddr, value: 148246800000000000});
+
+
     try {
-        web3.personal.unlockAccount(fromAddr, fromPass, 1);
+        web3.personal.unlockAccount(fromAddr, fromPass, 60);
     }
     catch (error){
         res.status(Static.HTTP_ERROR).send({msg : "Account Password incorrect"});
         return ;
     }
 
-    aboTokenContract.deployed.then( aboToken => {
+    aboTokenContract.deployed().then( aboToken => {
         aboToken.balanceOf.call(fromAddr).then(balance => {
             try{
                 if (balance < value){
@@ -128,16 +132,16 @@ app.post("/create/sendToken", (req, res, next) => {
                 res.status(Static.HTTP_ERROR).send({msg : error.message});
                 return ;
             }
-            
+
             return aboToken;
         }).then( aboToken => {
-            aboToken.approve(fromAddr, value, {from : Static.ORIGIN_ADDR}).then( result => {
+            aboToken.approve(Static.ORIGIN_ADDR, value, {from : fromAddr, gas: 7412340}).then( result => {
                 return aboToken;
             }).then( aboToken => {
                 aboToken.transferFrom(fromAddr, toAddr, value, {from : Static.ORIGIN_ADDR}).then( result => {
                     res.status(Static.HTTP_OK).send({txHash : result.tx});
                 });
-            });  
+            });
         });
     });
 });
@@ -146,50 +150,64 @@ app.post("/create/sendToken", (req, res, next) => {
 app.get("/get/bloodDoc", async (req, res, next) => {
     var contactAddr = req.query.contactAddr;
 
-    await aboContract.at(contactAddr).then( abo => {
-        abo.getBloodAgency.call().then( bloodAgency => {
-            BloodDoc.bloodAgency = bloodAgency;
-        })
-    })
-    await aboContract.at(contactAddr).then( abo => {
-        abo.getBloodDocID.call().then( bloodDocID => {
-            BloodDoc.bloodDocID = bloodDocID;
-        });
-    });
+    try{
 
-    await aboContract.at(contactAddr).then( abo => {
-        abo.getBloodingType.call().then( bloodingType => {
-            BloodDoc.bloodingType= bloodingType;
-        });
-    });
+    	await aboContract.at(contactAddr).then( abo => {
+            abo.getBloodAgency.call().then( bloodAgency => {
+                BloodDoc.bloodAgency = bloodAgency;
+            })
+    	});
 
-    await aboContract.at(contactAddr).then( abo => {
-        abo.getBloodAmount.call().then( bloodAmount => {
-            BloodDoc.bloodAmount = bloodAmount;
-        });
-    });
+	await aboContract.at(contactAddr).then( abo => {
+            abo.getBloodDocID.call().then( bloodDocID => {
+                BloodDoc.bloodDocID = bloodDocID;
+            });
+    	});
 
-    await aboContract.at(contactAddr).then( abo => {
-        abo.getRegTime.call().then( timestamp => {
-            BloodDoc.regTime = Func.translateTime(timestamp);
+    	await aboContract.at(contactAddr).then( abo => {
+            abo.getBloodingType.call().then( bloodingType => {
+                BloodDoc.bloodingType= bloodingType;
+            });
         });
-    });
 
-    res.status(Static.HTTP_OK).send({
-        bloodAgency : BloodDoc.bloodAgency,
-        bloodDocID : BloodDoc.bloodDocID,
-        bloodingType : BloodDoc.bloodingType,
-        bloodAmount : BloodDoc.bloodAmount,
-        regTime : BloodDoc.regTime
-    });
+        await aboContract.at(contactAddr).then( abo => {
+            abo.getBloodAmount.call().then( bloodAmount => {
+                BloodDoc.bloodAmount = bloodAmount;
+            });
+        });
+
+        await aboContract.at(contactAddr).then( abo => {
+            abo.getRegTime.call().then( timestamp => {
+                BloodDoc.regTime = Func.translateTime(timestamp);
+            });
+        });
+
+        res.status(Static.HTTP_OK).send({
+            bloodAgency : BloodDoc.bloodAgency,
+            bloodDocID : BloodDoc.bloodDocID,
+            bloodingType : BloodDoc.bloodingType,
+            bloodAmount : BloodDoc.bloodAmount,
+            regTime : BloodDoc.regTime
+        });
+    }
+    catch (error){
+        res.status(Static.HTTP_ERROR).send({msg : error.message});
+        return ;
+    }
 });
 
 app.get("/get/balance", (req, res, next) => {
     var addr = req.query.addr;
-    
-    aboTokenContract.deployed().then( aboToken => {
-        aboToken.balanceOf.call(addr).then( balance => {
-            res.status(Static.HTTP_OK).send({balance : balance.toString(10)});
+
+    try{
+    	aboTokenContract.deployed().then( aboToken => {
+            aboToken.balanceOf.call(addr).then( balance => {
+                res.status(Static.HTTP_OK).send({balance : balance.toString(10)});
+            });
         });
-    });
+    }
+    catch (error){
+        res.status(Static.HTTP_ERROR).send({msg: error.message});
+	return ;
+    }
 });
